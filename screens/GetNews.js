ba63d4f1f9e8a8c5e08e4cefe69062a9855d517f
Raman Sharma <rksraman73@gmail.com>
2022-09-05 20:48:13 +0530

    alll category by search data

--- screens/GetNews.js
+++ screens/GetNews.js
@@ -1,12 +1,93 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  Image,
+  ActivityIndicator,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
+
+const deviceHeight = Dimensions.get('window').height;
+const deviceWidth = Dimensions.get('window').width;
 
 class GetNews extends Component {
-  state = {};
+  state = {
+    news: [],
+  };
+
+  componentDidMount() {
+    this.props.navigation.setOptions({
+      title: this.props.route.params.category,
+    });
+
+    fetch(
+      `https://newsapi.org/v2/top-headlines?category=${this.props.route.params.category}&country=in&apiKey=9c5a58c96b214d468e8f20d5571dae83`,
+    )
+      .then(res => res.json())
+      .then(response => {
+        this.setState({
+          news: response.articles,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   render() {
     return (
-      <View>
-        <Text>{this.props.route.params.category}</Text>
+      <View style={{alignItems: 'center'}}>
+        {this.state.news.length === 0 ? (
+          <ActivityIndicator
+            style={{
+              height: deviceHeight,
+              width: deviceWidth,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            size="large"
+            color="black"
+          />
+        ) : (
+          <ScrollView showsVerticalScrollIndicator={false}>
+            {this.state.news.map((news, index) =>
+              news.urlToImage ? (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() =>
+                    this.props.navigation.navigate('WebView', {
+                      url: news.url,
+                    })
+                  }>
+                  <View
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                      backgroundColor: 'white',
+                      borderRadius: 10,
+                      elevation: 4,
+                      width: deviceWidth - 30,
+                      marginVertical: 7,
+                    }}>
+                    <Image
+                      source={{uri: `${news.urlToImage}`}}
+                      style={{height: 100, width: 100, borderRadius: 10}}
+                    />
+                    <Text
+                      style={{
+                        width: deviceWidth - 130,
+                        paddingLeft: 10,
+                        paddingTop: 5,
+                      }}>
+                      {news.title}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              ) : null,
+            )}
+          </ScrollView>
+        )}
       </View>
     );
   }
